f422650f3475325d90ce7f5b4ae53d215fabd630
Sean Hellum <seanhellum45@gmail.com>
2019-11-10 17:02:26 +0000

    test

---
+++ dijkstra.py
@@ -0,0 +1,29 @@
+GRAPH = {"A": {"B": 3, "C": 4, "E": 9, "F": 6},
+         "B": {"A": 3, "C": 5, "D": 7},
+         "C": {"A": 4, "B": 5},
+         "D": {"B": 7},
+         "E": {"B": 5, "A": 9},
+         "F": {"A": 6}}
+
+
+def bfs(graph, start, end):
+    queue = [(start, [start])]
+    while queue:
+        node, path = queue.pop(0)
+        for nextNode in graph[node]:
+            if nextNode in path:
+                continue
+            elif nextNode == end:
+                yield path + [nextNode]
+            else:
+                queue.append((nextNode, path + [nextNode]))
+def djikstra(graph, the_list, start):
+    thesum = 0
+    item = graph.get(start)
+    for x in item.values():
+        thesum += x
+    print(thesum)
+the_list = [x for x in bfs(GRAPH, 'A', 'D')]
+djikstra(GRAPH, the_list, "A")
+print(the_list)
+# print(the_list)
